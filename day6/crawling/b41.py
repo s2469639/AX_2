01f77b92e24dee24c5165db6dcb2188603c3a5d4
s2469639 <sy2469639@gmail.com>
2026-09-14 16:26:46 +0900

    ㅌ
    ㅌ

--- day6/crawling/b41.py
+++ day6/crawling/b41.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-reponse = requests.get(https://www.google.com)
+response = requests.get("https://www.google.com")
 
-html = 
-soup = BeautifulSoup(html, "html.")
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
